6707fd79c862d0dd1898ffa968436a57b9aa9eaa
App dev <appdev@Apps-MacBook-Air.local>
2020-12-15 09:06:24 +0530

    react_tictactoe_1

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Game from './components/Game';
+import Square from './components/Square';
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+      <Square />
+    </div>
+  );
+}
+
+export default App;
